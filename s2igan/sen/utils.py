5a362c734a16f9ce6b40adabbea93fff12cd2dd6
DadRin <xuandienk4@gmail.com>
2024-04-22 15:48:38 +0700

    s2igan

--- s2igan/sen/utils.py
+++ s2igan/sen/utils.py
@@ -163,7 +163,8 @@ def sed_train_epoch(
         A = speech_encoder(specs, len_specs)
         cls_speech = classifier(A)
 
-        dist_loss, loss = criterion(cls_speech, labels)
+        # dist_loss, loss = criterion(cls_speech, labels)
+        dist_loss, loss = criterion(cls_speech.unsqueeze(0), labels)
         loss.backward()
 
         optimizer.step()
@@ -219,7 +220,8 @@ def sed_eval_epoch(
             A = speech_encoder(specs, len_specs)
             cls_speech = classifier(A)
 
-            dist_loss, loss = criterion( cls_speech, labels)
+            # dist_loss, loss = criterion( cls_speech, labels)
+            dist_loss, loss = criterion(cls_speech.unsqueeze(0), labels)
 
             loss = loss.item()
 
